578b1ca6c2fed565cb74efb66c8505e17f9eb63a
lexkotov <93532432+lexkotov@users.noreply.github.com>
2022-06-02 23:00:58 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,80 +1,131 @@
-package Lesson3;
+package Lesson4;
+
+import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String args[]) {
-        Number1();
-        Number2();
-        Number3();
-        Number4();
+
+    public static void main(String[] args) {
+        String[][] field = initField(3);
+        printField(field);
+
+        while (true) {
+
+            humanTurn(field);
+            printField(field);
+            if (isDraw(field)) {
+                break;
+            }
+            if (isWinner(field, "X")) {
+                System.out.println("Human win");
+                break;
+            }
+
+
+            aiTurn(field);
+            printField(field);
+            if (isDraw(field)) {
+                break;
+            }
+            if (isWinner(field, "0")) {
+                System.out.println("PC Win");
+                break;
+            }
+
+        }
+        System.out.println("Game Over");
     }
 
-    public static void Number1() {
-        int[] num = {1, 1, 0, 0, 1, 0, 1, 1, 0, 1};
-        int num1 = num.length;
-
-        for (int i = 0; i < num1; i++) {
-            System.out.print(i + " " + num[i]);
+    public static String[][] initField(int size) {
+        String[][] field = new String[size][size];
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                field[i][j] = "*";
+            }
         }
-        System.out.println(" ");
-        for (int i = 0; i < num1; i++)
-            if (num[i] == 1) {
-                num[i] = 0;
-            } else num[i] = 1;
-        for (int i = 0; i < num1; i++) {
-            System.out.print(i + " " + num[i]);
-        }
+        return field;
     }
 
-    public static void Number2() {
-        int[] a = new int[100];
-        int b = 0;
-        int c = a.length;
-        for (int i = 0; i < c; ++i, b = b + 1) {
-            a[i] = b;
-            System.out.println(b);
-
-        }
-    }
-
-    public static void Number3() {
-        int[] a = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1};
-        int b = a.length;
-        for (int i = 0; i < b; i++) {
-            if (a[i] < 6)
-                a[i] = a[i] * 2;
-        }
-        for (int i = 0; i < b; i++) {
-
-            System.out.print(a[i] + " ");
-
-        }
-    }
-
-    public static void Number4() {
-
-        int[][] cube = new int[6][6];
-
-        for (int a = 0; a < cube[0].length; a++) {
-            cube[a][a] = 1;
-        }
-        for (int a = 0; a < cube[0].length; a++) {
-            cube[cube.length - 1][a] = 1;
-        }
-        for (int a = 0; a < cube.length; a++) {
-            cube[a][0] = 1;
-        }
-        for (int a = 0; a < cube[0].length; a++) {
-            cube[a][cube[a].length - 1] = 1;
-        }
-
-
-        for (int a = 0; a < cube.length; a++) {
-            for (int b = 0; b < cube[a].length; b++) {
-                System.out.print(cube[a][b] + " ");
+    public static void printField(String[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                System.out.print(field[i][j] + " ");
             }
             System.out.println();
         }
+        System.out.println("-----");
+    }
 
+    public static void humanTurn(String[][] field) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Clik point x  y: ");
+            int x = scanner.nextInt()-1;
+            int y = scanner.nextInt()-1;
+            int size = field.length;
+
+            if (x < size && y < size && x >= 0 && y >= 0) {
+
+                if (field[x][y].equals("*")) {
+                    field[x][y] = "X";
+                    break;
+                } else {
+                    System.out.println("11");
+                }
+
+            } else {
+                System.out.println("No game place");
+            }
+
+        }
+    }
+
+    public static void aiTurn(String[][] field) {
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            Random random = new Random();
+            int x = random.nextInt(field.length);
+            int y = random.nextInt(field.length);
+            if (field[x][y].equals("*")) {
+                field[x][y] = "0";
+                break;
+            }
+        }
+        System.out.println("PC go to:)");
+    }
+
+    public static boolean isDraw(String[][] field) {
+        for (int i = 0; i < field.length; i++) {
+            for (int j = 0; j < field[0].length; j++) {
+                if (field[i][j].equals("*")) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public static boolean isWinner(String[][] field, String turn) {
+
+        if (field[0][0].equals(turn) && field[0][1].equals(turn) && field[0][2].equals(turn) ||
+                field[1][0].equals(turn) && field[1][1].equals(turn) && field[1][2].equals(turn) ||
+                field[2][0].equals(turn) && field[2][1].equals(turn) && field[2][2].equals(turn)
+        ) {
+            return true;
+        }
+
+        else if (field[0][0].equals(turn) && field[1][0].equals(turn) && field[2][0].equals(turn) ||
+                field[0][1].equals(turn) && field[1][1].equals(turn) && field[2][1].equals(turn) ||
+                field[0][2].equals(turn) && field[1][2].equals(turn) && field[2][2].equals(turn)) {
+            return true;
+        }
+        
+        else if (field[0][0].equals(turn) && field[1][1].equals(turn) && field[2][2].equals(turn) ||
+                field[2][0].equals(turn) && field[1][1].equals(turn) && field[0][2].equals(turn)) {
+            return true;
+        }
+
+        return false;
     }
 
 }
